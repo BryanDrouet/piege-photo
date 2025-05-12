c2672986e9d6cf44122e2aac13f1d0fbe3c32051
Bryan_Drouet <77234743+BryanDrouet@users.noreply.github.com>
2025-05-12 11:20:14 +0200

    Update 404.php

--- 404.php
+++ 404.php
@@ -8,8 +8,8 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <title>404 - Page non trouvée</title>
-    <link rel="icon" type="image/png" href="/PiegePhoto/assets/icon.png">
-    <link rel="stylesheet" href="/PiegePhoto/style.css">
+    <link rel="icon" type="image/png" href="/assets/icon.png">
+    <link rel="stylesheet" href="/style.css">
 </head>
 
 <body>
@@ -18,7 +18,7 @@ session_start();
         <span class="go-back-arrow" onclick="history.back()">←</span>
         <h1 class="error-title">404</h1>
         <p class="error-message">La page que vous recherchez est introuvable.</p>
-        <p><a href="/PiegePhoto/home.php" class="go-home">Retour à la page d'accueil</a></p>
+        <p><a href="/home.php" class="go-home">Retour à la page d'accueil</a></p>
     </div>
 </body>
 
